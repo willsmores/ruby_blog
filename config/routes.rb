0c6ee1017d43d5e9ecd8bbe7645ed6fcb6002328
Steve Willsmore <scwillsmore@gmail.com>
2023-01-24 13:04:48 -0500

    Refactor article routes using resources method and link_to helper.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root "articles#index"
 
-  get "/articles", to: "articles#index"
-  get "/articles/:id", to: "articles#show"
+  # Refactored to use Rails routes method 'resources'
+  resources :articles
+  # get "/articles", to: "articles#index"
+  # get "/articles/:id", to: "articles#show"
 end
