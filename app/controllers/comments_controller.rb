dea7495d1dc11c8d0adbd1feebb3dbdce2c4f253
Steve Willsmore <scwillsmore@gmail.com>
2023-01-24 15:55:19 -0500

    Add basic authentication for interacting with articles.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,6 @@
 class CommentsController < ApplicationController
+  http_basic_authenticate_with name: "dhh", password: "secret", only: :destroy
+  
   def create
     # Each comment keeps track of which article it relates to
     @article = Article.find(params[:article_id])
