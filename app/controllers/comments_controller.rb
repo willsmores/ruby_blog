bc3b8954bf6cc15ce9c69a46979f7813607192ef
Steve Willsmore <scwillsmore@gmail.com>
2023-01-24 14:31:09 -0500

    Add comments routing, display on 'show' page.

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,14 @@
+class CommentsController < ApplicationController
+  def create
+    # Each comment keeps track of which article it relates to
+    @article = Article.find(params[:article_id])
+    # Create method to create/save the comment
+    @comment = @article.comments.create(comment_params)
+    redirect_to article_path(@article)
+  end
+
+  private
+    def comment_params
+      params.require(:comment).permit(:commenter, :body)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   root "articles#index"
 
+  resources :articles do
+    resources :comments
+  end
   # Refactored to use Rails routes method 'resources'
-  resources :articles
   # get "/articles", to: "articles#index"
   # get "/articles/:id", to: "articles#show"
 end
