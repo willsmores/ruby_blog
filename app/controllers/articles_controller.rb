afc70903d2809a588e6a27ffc82d6d9c9da733a5
Steve Willsmore <scwillsmore@gmail.com>
2023-01-24 13:18:54 -0500

    Add routing and and form for new articles.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,20 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  # Instantiates a new article
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(title: "...", body: "...")
+
+    if @article.save
+      # Redirects to http://localhost:3000/articles/#{@article.id}
+      redirect_to @article
+    else
+      render :new
+    end
+  end
 end
